093ceb901b5426e1ecaa7c9fb367cadd36e1de7f
Max <tutv95@gmail.com>
2016-12-24 23:38:16 +0700

    Updated connect database

--- app.databases.js
+++ app.databases.js
@@ -1,6 +1,17 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/slackato');
 
-global.mongodb = mongoose;
+module.exports = function () {
+    let uri = process.env.APP_DB || 'mongodb://localhost/slackato';
+
+    return new Promise((resolve, reject) => {
+        mongoose.connect(uri, function (error) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve('Connected');
+            }
+        });
+    });
+};
--- index.js
+++ index.js
@@ -5,15 +5,26 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-require('./app.databases');
+let oathCtrl = require('./controllers/oauth');
+app.get('/oauth', oathCtrl.callback);
+
+const database = require('./app.databases');
+database()
+    .then(
+        () => {
+            console.log('Database connected');
+            listen();
+        },
+        error => {
+            console.log(error);
+        }
+    );
 
 app.use(express.static(__dirname + '/public'));
 
-let oathCtrl = require('./controllers/oauth');
-
-app.get('/oauth', oathCtrl.callback);
-
-let port = process.env.HOST_PORT || 7878;
-app.listen(port, () => {
-    console.log('App listening on port ' + port);
-});
+function listen() {
+    let port = process.env.HOST_PORT || 7878;
+    app.listen(port, () => {
+        console.log('App listening on port ' + port);
+    });
+}
