b24fe962777e5599d70a73e6edc2d3ddc6160046
Max <tutv95@gmail.com>
2016-12-24 23:22:50 +0700

    Add page access denied

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -3,8 +3,13 @@
 let slackSrv = require('../service/slack.api');
 
 module.exports.callback = function (req, res, next) {
-    let code = req.query.code || false;
+    let error = req.query.error || false;
+    if (error) {
+        res.redirect('/denied.html');
+        return;
+    }
 
+    let code = req.query.code || false;
     slackSrv.getToken(code)
         .then(
             response => {
