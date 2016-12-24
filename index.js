d3edc1a5b0820e2ea7a0027311b8106ebc2a1ab9
Max <tutv95@gmail.com>
2016-12-24 16:57:44 +0700

    Test

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+let oathCtrl = require('./controllers/oauth');
+
+app.get('/', (req, res, next) => {
+    res.send('Hello');
+});
+
+app.get('/oauth', oathCtrl.callback);
+
 let port = process.env.HOST_PORT || 7878;
 app.listen(port, () => {
     console.log('App listening on port ' + port);
