4fbb67f306fd03337099e3a4cb0461628d79b91c
Max <tutv95@gmail.com>
2017-01-29 23:04:46 +0700

    Refactor code

--- service/slack.hook.js
+++ service/slack.hook.js
@@ -6,8 +6,6 @@ module.exports.send = function (url, data) {
     let deferred = Promise.defer();
     data = JSON.stringify(data);
 
-    console.log(`Send to hook: ${url}\n`, data);
-
     request.post(url, {form: data}, (error, response, body) => {
         if (error) {
             return deferred.reject(error);
