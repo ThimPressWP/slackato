30524d1693de1c7a3a4f4dfee36b083fa8311bc5
Max <tutv95@gmail.com>
2018-03-19 11:13:53 +0700

    Updated app

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(session({
 /**
  * Config serve static files.
  */
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '/../public'));
 
 /**
  * Config routes.
