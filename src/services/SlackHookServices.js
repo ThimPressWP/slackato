faf7d201c67e8759f2eb072a0d301264a0bc7d0c
Max <tutv95@gmail.com>
2018-07-12 13:53:36 +0700

    test

--- src/services/SlackHookServices.js
+++ src/services/SlackHookServices.js
@@ -1,8 +1,6 @@
 const request = require('request-promise-native');
 
 module.exports.send = function (url, data) {
-    data = JSON.stringify(data);
-
     return request({
         method: 'POST',
         uri: url,
